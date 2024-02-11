f0bb03e98b3e81d2cbef3d27537e50856332ce7a
hazylua <40061395+hazylua@users.noreply.github.com>
2024-02-10 23:07:03 -0300

    Change theme config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
+import { createThemes } from 'tw-colors';
+import { dark, light } from './themes/index.cjs';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: 'class',
   content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {}
-  },
-  plugins: [require('@tailwindcss/forms')]
+  plugins: [
+    require('@tailwindcss/forms'),
+    createThemes({
+      dark,
+      light
+    })
+  ]
 };
---
+++ themes/index.cjs
@@ -0,0 +1,4 @@
+module.exports = {
+  dark: require('./dark.json'),
+  light: require('./light.json')
+};
